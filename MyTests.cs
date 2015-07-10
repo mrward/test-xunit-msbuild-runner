61cfb7acebc932389c46adee0f08ecf46f3c5a63
Matt Ward <ward.matt@gmail.com>
2015-07-10 14:37:37 +0100

    Add xunit test project.

---
+++ MyTests.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using Xunit;
+
+namespace XUnitTestFooBar
+{
+	public class MyTests
+	{
+		[InlineData(3)]
+		[InlineData(5)]
+		[InlineData(6)]
+		public void MyFirstTheory(int value)
+		{
+			Assert.True(IsOdd(value));
+		}
+
+		bool IsOdd(int value)
+		{
+			return value % 2 == 1;
+		}
+	}
+}
+
